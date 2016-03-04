13a9ac524c5c87540b05930a8b263b38eab17e2e
张大漾丨前端开发 <1417376255@qq.com>
2016-03-04 18:15:02 +0800

    add button navbar

---
+++ bootstrap-for-designer/res/js/yhtml5.js
@@ -0,0 +1,8 @@
+$(function  () {
+	$('.js-nav-navbar-full').click(function  () {
+		$('.custom-navbar').toggleClass('navbar-full')
+	})
+	$('.js-nav-bg-success').click(function  () {
+		$('.custom-navbar').toggleClass('bg-success')
+	})
+})
