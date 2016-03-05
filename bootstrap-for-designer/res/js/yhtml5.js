496d3e6123a898005687d3e1d16c2e058cfb7330
张大漾丨前端开发 <1417376255@qq.com>
2016-03-05 14:15:41 +0800

    add button style

--- bootstrap-for-designer/res/js/yhtml5.js
+++ bootstrap-for-designer/res/js/yhtml5.js
@@ -5,4 +5,11 @@ $(function  () {
 	$('.js-nav-bg-success').click(function  () {
 		$('.custom-navbar').toggleClass('bg-success')
 	})
+	$('.js-btn-lg').click(function  () {
+		$('.custom-button').remove('btn-sm')
+//		$('.custom-button').toggleClass('btn-lg')
+	})
+	$('.js-btn-sm').click(function  () {
+		$('.custom-button').toggleClass('btn-sm')
+	})
 })
