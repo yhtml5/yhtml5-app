ad106a76766b5dc70f99d15f9bac2be21f0e0c39
张大漾丨前端开发 <1417376255@qq.com>
2016-03-19 14:33:49 +0800

    add new button

--- sdk-console/development/components/head/head.js
+++ sdk-console/development/components/head/head.js
@@ -17,21 +17,21 @@ y(function() {
 		y('.nav .y-active').removeClass('active')
 		y(this).addClass('active')
 	})
-	y('.breadcrumb .j-app li').click(function() {
-		y('.breadcrumb .j-app li').removeClass('active')
+	y('.j-dashboard-breadcrumb .j-app li').click(function() {
+		y('.j-dashboard-breadcrumb .j-app li').removeClass('active')
 		y(this).addClass('active')
 	})
-	y('.breadcrumb .j-function li').click(function() {
-		y('.breadcrumb .j-function li').removeClass('active')
+	y('.j-dashboard-breadcrumb .j-function li').click(function() {
+		y('.j-dashboard-breadcrumb .j-function li').removeClass('active')
 		y(this).addClass('active')
 	})
-	y('.j-panel-header-function .dropdown-menu a').click(function() {
+	y('.j-dashboard-header-function .dropdown-menu a').click(function() {
 		var a = y(this).text()
-		y('.j-panel-header-function span').text(a)
+		y('.j-dashboard-header-function span').text(a)
 	})
-	y('.j-panel-header-app .dropdown-menu a').click(function() {
+	y('.j-dashboard-header-app .dropdown-menu a').click(function() {
 		var a = y(this).text()
-		y('.j-panel-header-app span').text(a)
+		y('.j-dashboard-header-app span').text(a)
 	})
 	y(".y-app-add-create").on('click', function() {
 		var btn = y(this).button('loading')
@@ -62,10 +62,42 @@ y(function() {
 		}, 1000)
 	})
 
+	function yDashboardPaymentFilter() {
+		var a = y('.y-dashboard-payment-filter').width()
+
+		function small() {
+			y('.y-dashboard-payment-filter .j-date').text("日期")
+			y('.y-dashboard-payment-filter .j-pay').text("支付")
+			y('.y-dashboard-payment-filter .j-settlement').text("结算")
+			y('.y-dashboard-payment-filter .j-channel').text("渠道")
+		}
+
+		function big() {
+			y('.y-dashboard-payment-filter .j-date').html("选择日期&nbsp;")
+			y('.y-dashboard-payment-filter .j-pay').html("支付状态&nbsp;")
+			y('.y-dashboard-payment-filter .j-settlement').html("结算状态&nbsp;")
+			y('.y-dashboard-payment-filter .j-channel').html("全部渠道&nbsp;")
+		}
+		if (a < 388) {
+			small()
+		}
+		y(window).resize(function() {
+			var w = y('.y-dashboard-payment-filter').width()
+			if (w < 388) {
+				small()
+			} else {
+				big()
+			}
+			console.log(w)
+			console.log("不要调皮地频繁调节窗口")
+		})
+	}
+	yDashboardPaymentFilter()
+
 	function yExpandTable() {
-		var tbody = y('.y-member tbody').html()
+		var tbody = y('.y-member tbody,.y-dashboard-payment tbody').html()
 		for (var i = 0; i < 9; i++) {
-			y('.y-member tbody').append(tbody)
+			y('.y-member tbody,.y-dashboard-payment tbody').append(tbody)
 		}
 	}
 	yExpandTable();
