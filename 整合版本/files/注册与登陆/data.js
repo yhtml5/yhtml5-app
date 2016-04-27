ec5c65900e67ef1838afa1e27b5a4da810aa751a
张大漾丨前端开发 <1417376255@qq.com>
2016-04-27 16:35:16 +0800

    add production

---
+++ 整合版本/files/注册与登陆/data.js
@@ -0,0 +1,71 @@
+﻿$axure.loadCurrentPage({
+  "url":"注册与登陆.html",
+  "generationDate":new Date(1461745744439.68),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"7143fdd528774fbe8bc4376be778c4d9",
+    "type":"Axure:Page",
+    "name":"注册与登陆",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"应用字体",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[{
+          "id":"1979d5c7ec364b6abfd9b553d63a3c9b",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":200,
+              "y":200},
+            "size":{
+              "width":1286,
+              "height":687}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"d54bdd744fe44aed810b24916fade621",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":200,
+                  "y":200},
+                "size":{
+                  "width":1286,
+                  "height":687}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/注册与登陆/u0.png"}}]}},
+  "masters":{
+},
+  "objectPaths":{
+    "1979d5c7ec364b6abfd9b553d63a3c9b":{
+      "scriptId":"u0"},
+    "d54bdd744fe44aed810b24916fade621":{
+      "scriptId":"u1"}}});
