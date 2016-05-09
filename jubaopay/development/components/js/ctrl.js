92f3c5e9b8a14d44a1ab44d0046ffe75f93fedda
张大漾丨前端开发 <1417376255@qq.com>
2016-05-09 15:14:31 +0800

    update

--- jubaopay/development/components/js/ctrl.js
+++ jubaopay/development/components/js/ctrl.js
@@ -2,18 +2,29 @@
 
 angular.module('controllers', ['ui.bootstrap', 'ngAnimate'])
     .controller('controllers', function($scope, $uibModal) {
-        $scope.animationsEnabled = true;
-        $scope.smsConfirmOpen = function(size) {
-            var modalInstance = $uibModal.open({
-                animation: $scope.animationsEnabled,
-                templateUrl: 'smsConfirm.html',
-                controller: 'smsConfirmCtrl',
-                size: size
-            })
-        }
-    })
-    .controller('smsConfirmCtrl', function($scope, $uibModalInstance) {
-        $scope.cancel = function() {
-            $uibModalInstance.dismiss('cancel');
-        }
+        $scope.downloadRecord = [{
+            "sdkName": "JAVA包",
+            "sdkNote": "（PC支付）",
+            "sdkClass": "icon-iconanzhuo"
+        }, {
+            "sdkName": ".NET包",
+            "sdkNote": "（PC支付）",
+            "sdkClass": "icon-iconanzhuo"
+        }, {
+            "sdkName": "PHP包",
+            "sdkNote": "（PC支付）",
+            "sdkClass": "icon-iconanzhuo"
+        }, {
+            "sdkName": "COCS2D包",
+            "sdkNote": "（SDK开发包）",
+            "sdkClass": "icon-iconanzhuo"
+        }, {
+            "sdkName": "IOS包",
+            "sdkNote": "（SDK开发包）",
+            "sdkClass": "icon-iconanzhuo"
+        }, {
+            "sdkName": "商户接入文档",
+            "sdkNote": "下载文档",
+            "sdkClass": "icon-iconanzhuo"
+        }]
     })
