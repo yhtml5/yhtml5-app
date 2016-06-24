1827e121d0b0a20248588c136b6d90fd08744474
张大漾丨前端开发 <1417376255@qq.com>
2016-06-24 10:55:39 +0800

    11

--- dashboard/app-vm/components/js/vm.js
+++ dashboard/app-vm/components/js/vm.js
@@ -1,12 +1,13 @@
-$(function() {
-    $(".form_datetime").datetimepicker({
-        language: 'zh-CN',
-        format: "dd MM yyyy",
-        autoclose: true,
-        todayBtn: true,
-        inline: true
-    });
-    $('.dropdown-menu li').click(function() {
-        $('.j-pay-btn,.j-settlement,.j-channel,.j-settlement').text($(this).text());
-    });
-})
+'use strict';
+//$(function() {
+//  $(".form_datetime").datetimepicker({
+//      language: 'zh-CN',
+//      format: "dd MM yyyy",
+//      autoclose: true,
+//      todayBtn: true,
+//      inline: true
+//  });
+//  $('.dropdown-menu li').click(function() {
+//      $('.j-pay-btn,.j-settlement,.j-channel,.j-settlement').text($(this).text());
+//  });
+//})
