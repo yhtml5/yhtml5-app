803990cb29e6c15d938b6fdb1357ca78e4fa3eb7
张大漾丨前端开发 <1417376255@qq.com>
2016-07-07 12:05:22 +0800

    run node

---
+++ dashboard/output/vm/V0.4-070704/view/appList/appList.ctrl.js
@@ -0,0 +1,29 @@
+'use strict';
+angular.module('yhtml5.appList', ['ui.bootstrap', 'ngAnimate'])
+    .controller('yhtml5.appList', function($scope, $http, $uibModal) {
+        $scope.animationsEnabled = true;
+        $scope.open = function(size) {
+            var modalInstance = $uibModal.open({
+                backdrop: 'static',
+                animation: $scope.animationsEnabled,
+                templateUrl: 'appList-add.html',
+                controller: 'ModalInstanceCtrl',
+                size: size
+            })
+        };
+        $scope.toggleAnimation = function() {
+            $scope.animationsEnabled = !$scope.animationsEnabled;
+        };
+    })
+    .controller('ModalInstanceCtrl', function($state, $scope, $http, $uibModalInstance, $uibModal, $state) {
+        $scope.userAppFormSave = function(size) {
+            $scope.userPersonFormEnabled = true;
+            $scope.userPersonUpdate = true;
+            $scope.userPersonSave = true;
+            $uibModalInstance.dismiss('cancel');
+            $state.reload('dashboard');
+        };
+        $scope.cancel = function() {
+                $uibModalInstance.dismiss('cancel');
+        }
+    });
