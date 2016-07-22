775bbcee0283b1d968316ac6f0f794fef2d9d06a
chenyexing <475249948@qq.com>
2016-07-22 19:09:02 +0800

    11

--- crm/app/components/js/do.js
+++ crm/app/components/js/do.js
@@ -6,10 +6,10 @@
 
 $(function () {
     $("[y-popoper='uploadQRcode'] a").mouseenter(function () {
-        $("[y-popoper='ewmsml']").fadeIn();
+        $("[y-popoper='ewmsml'] img").fadeIn();
     });
     $("[y-popoper='uploadQRcode'] a").mouseleave(function () {
-        $("[y-popoper='ewmsml']").fadeOut();
+        $("[y-popoper='ewmsml'] img").fadeOut();
     });
     $("[y-popoper='noopsycheMoney']").mouseenter(function () {
         $("[y-popoper='infoUl']").fadeIn();
