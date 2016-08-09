93191cfd43dc1e3a468163396a696d1eb4976e8e
张大漾丨前端开发 <1417376255@qq.com>
2016-08-09 09:53:23 +0800

    update

--- sdk/app/components/js/vue.js
+++ sdk/app/components/js/vue.js
@@ -4,35 +4,53 @@
 /************  vue  ************/
 
 var imgAlipay = __inline('../public/yhklogo.png');
-var imgCard = __inline('../public/zfblogo.png');
-var imgWechat = __inline('../public/wxlogo.png');
-var imgEmpty = __inline('../public/konglogo.png');
 var imgArrowRight = __inline('../public/arrow.png');
+var imgCard = __inline('../public/zfblogo.png');
+var imgEmpty = __inline('../public/konglogo.png');
+var imgLogo = __inline('../public/jbylogo_58px.png');
+var imgWechat = __inline('../public/wxlogo.png');
 
-new Vue({
-    el: '#yhtml5',
-    data: {
-        imgLogo: imgLogo,
-        imgEmpty: imgEmpty,
-        imgArrowRight: imgArrowRight,
-        name: '测试商品',
-        money: '$55.00',
-        channels: [
-            {
-                img: imgAlipay,
-                name: '支付宝',
-                text: '推荐支付宝用户'
-            }, {
-                img: imgWechat,
-                name: '微信',
-                text: '推荐微信用户'
-            }, {
-                img: imgCard,
-                name: '信用卡',
-                text: '推荐使用信用卡'
-            }],
-        copyright: '该服务由聚宝云计费提供',
-        tel: '如果支付问题请拨打 0571-86800282',
-        list: false
+var yhtml5Data = {
+    appId:000001,
+    imgLogo: imgLogo,
+    imgEmpty: imgEmpty,
+    imgArrowRight: imgArrowRight,
+    name: '测试商品',
+    money: '$55.00',
+    channels: [
+        {
+            img: imgAlipay,
+            name: '支付宝',
+            text: '推荐支付宝用户'
+        },
+        {
+            img: imgWechat,
+            name: '微信',
+            text: '推荐微信用户'
+        },
+        {
+            img: imgCard,
+            name: '信用卡',
+            text: '推荐使用信用卡'
+        }
+    ],
+    copyright: '该服务由聚宝云计费提供',
+    tel: '如果支付问题请拨打 0571-86800282'
+}
+
+function isList() {
+    if (yhtml5Data.channels === '') {
+        yhtml5Data.isList = false
+    } else {
+        yhtml5Data.isList = true
     }
+    return isList
+}
+isList()
+console.log(yhtml5Data.isList)
+
+var yhtml5VM = new Vue({
+    el: '#yhtml5',
+    data: yhtml5Data
 });
+
